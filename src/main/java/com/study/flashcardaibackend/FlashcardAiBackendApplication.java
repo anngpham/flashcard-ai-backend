691c910c1714b8766cc9a471f16ba929490e5256
An Pham <anpham@Ans-MacBook-Air.local>
2024-07-06 21:19:13 +0700

    feat: init flashcard AI project

---
+++ src/main/java/com/study/flashcardaibackend/FlashcardAiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.study.flashcardaibackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardAiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlashcardAiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/flashcardaibackend/FlashcardAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.flashcardaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
