1d593b88ac4d448dabe4ffe9d23f5921d988076e
anpn <anthachby2@gmail.com>
2024-07-09 00:16:46 +0700

    refactor: add comment and change notnull to notblank

--- src/main/java/com/study/flashcardaibackend/security/jwt/JwtConstants.java
+++ src/main/java/com/study/flashcardaibackend/security/jwt/JwtConstants.java
@@ -6,5 +6,5 @@ public class JwtConstants {
 
     public static final String TOKEN_PREFIX = "Bearer";
 
-    public static final long EXPIRATION_TIME = 30 * 60 * 1000;
+    public static final long EXPIRATION_TIME = 30 * 60 * 1000; // 30 minutes
 }
