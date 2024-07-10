91738a18b97c7005de072131f5ec291617fbc23d
anpn <anthachby2@gmail.com>
2024-07-10 07:30:01 +0700

    refactor: add validation for description

--- src/main/java/com/study/flashcardaibackend/dto/SetRequest.java
+++ src/main/java/com/study/flashcardaibackend/dto/SetRequest.java
@@ -1,5 +1,7 @@
 package com.study.flashcardaibackend.dto;
 
+import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,5 +13,7 @@ public class SetRequest {
     @NotBlank(message = "title not blank")
     private String title;
 
+    @Nullable
+    @Max(value = 100, message = "description max 100 characters")
     private String description;
 }
