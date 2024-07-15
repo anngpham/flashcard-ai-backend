4bbff1a7b0f06bf103d1ec9e9e60ea128d9c47de
anpn <anthachby2@gmail.com>
2024-07-15 10:56:00 +0700

    fix: change argon2 to bcrypt encoder

--- src/main/java/com/study/flashcardaibackend/util/HashingUtil.java
+++ src/main/java/com/study/flashcardaibackend/util/HashingUtil.java
@@ -1,31 +1,20 @@
 package com.study.flashcardaibackend.util;
 
-import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 public final class HashingUtil {
-    private static final int SALT_LENGTH = 16; // Generate a 16 byte salt
-    private static final int HASH_LENGTH = 32; // Generate a 32 byte (256 bit) hash
-    private static final int PARALLELISM = 1; // Use 1 threads
-    private static final int MEMORY = 60000; // Use 64 MB of memory
-    private static final int ITERATIONS = 10; // Run 3 iterations
+    private static final int STRENGTH = 12;
 
     // Use Argon2PasswordEncoder to hash and verify passwords
-    private static final Argon2PasswordEncoder argonPwEncoder = new Argon2PasswordEncoder(SALT_LENGTH, HASH_LENGTH, PARALLELISM, MEMORY, ITERATIONS);
+    private static final BCryptPasswordEncoder bCryptPwEncoder = new BCryptPasswordEncoder(STRENGTH);
 
     public static String hashPassword(String rawPassword) {
-        return argonPwEncoder.encode(rawPassword);
+        return bCryptPwEncoder.encode(rawPassword);
     }
 
     public static boolean verifyPassword(String inputPassword, String hashPassword) {
-        return argonPwEncoder.matches(inputPassword, hashPassword);
+        return bCryptPwEncoder.matches(inputPassword, hashPassword);
     }
 
-    public static String hashPassword2(String rawPassword) {
-        return rawPassword;
-    }
-
-    public static boolean verifyPassword2(String inputPassword, String hashPassword) {
-        return inputPassword.equals(hashPassword);
-    }
 }
