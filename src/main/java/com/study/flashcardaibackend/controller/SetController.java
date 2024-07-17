65ec2dbbd52ba04f65c93e5bba2666e2bcff5798
anpn <anthachby2@gmail.com>
2024-07-17 10:14:51 +0700

    fix: detach sort to sortBy and sortDirection

--- src/main/java/com/study/flashcardaibackend/controller/SetController.java
+++ src/main/java/com/study/flashcardaibackend/controller/SetController.java
@@ -1,19 +1,17 @@
 package com.study.flashcardaibackend.controller;
 
-import com.study.flashcardaibackend.constant.ErrorConstants;
 import com.study.flashcardaibackend.constant.FilterAttrConstants;
 import com.study.flashcardaibackend.constant.PathConstants;
 import com.study.flashcardaibackend.dto.set.SetCreationBodyDTO;
 import com.study.flashcardaibackend.dto.set.SetCreationResponseDTO;
 import com.study.flashcardaibackend.dto.set.SetUpdateBodyDTO;
 import com.study.flashcardaibackend.dto.set.SetUpdateResponseDTO;
-import com.study.flashcardaibackend.exception.HttpRuntimeException;
 import com.study.flashcardaibackend.model.set.Set;
 import com.study.flashcardaibackend.service.set.SetService;
-import com.study.flashcardaibackend.util.StringUtil;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -25,7 +23,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -66,24 +63,15 @@ public class SetController {
     @GetMapping
     public ResponseEntity<Page<Set>> getListOfSet(
             HttpServletRequest request,
-            @RequestParam(defaultValue = "1") @Min(1) int page,
-            @RequestParam(value = "page_size", defaultValue = "20") @Min(1) int pageSize,
-            @RequestParam(value = "sort_by", defaultValue = "updated_at,desc") String sortBy,
-            @RequestParam(value = "search", defaultValue = "") @Size(max = 10) String search
+            @RequestParam(value = "page", defaultValue = "1", required = false) @Min(1) int page,
+            @RequestParam(value = "pageSize", defaultValue = "20") @Min(1) int pageSize,
+            @RequestParam(value = "sortBy", defaultValue = "updatedAt", required = false) @Pattern(regexp = "updatedAt|createdAt") String sortBy,
+            @RequestParam(value = "sortDirection", defaultValue = "desc", required = false) @Pattern(regexp = "asc|desc") String sortDirection,
+            @RequestParam(value = "search", defaultValue = "", required = false) @Size(max = 10) String search
 
     ) {
 
-        List<String> sortFields = List.of(sortBy.split(","));
-        if (sortFields.size() != 2) {
-            throw new HttpRuntimeException(HttpStatus.BAD_REQUEST, ErrorConstants.SORT_BY_PARAMETER_INVALID);
-        }
-        if (!(sortFields.getFirst().equals("created_at") || sortFields.getFirst().equals("updated_at"))) {
-            throw new HttpRuntimeException(HttpStatus.BAD_REQUEST, ErrorConstants.SORT_BY_PARAMETER_INVALID);
-        }
-        if (!(sortFields.getLast().equals("asc") || sortFields.getLast().equals("desc"))) {
-            throw new HttpRuntimeException(HttpStatus.BAD_REQUEST, ErrorConstants.SORT_BY_PARAMETER_INVALID);
-        }
-        Sort sort = Sort.by(Sort.Direction.fromString(sortFields.getLast()), StringUtil.convertSnakeToCamel(sortFields.getFirst()));
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
         Pageable pageable = PageRequest.of(page - 1, pageSize, sort);
 
         UUID userId = (UUID) request.getAttribute(FilterAttrConstants.USER_ID);
